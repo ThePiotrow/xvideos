365d1853d62304ad9c2e11c586d72bf21ffcfbd2
Maxime MARCHAND <mmarchand6@myges.fr>
2023-10-10 19:29:50 +0200

    Ça commence à fonctionner v2.2.3

--- front/src/pages/user/ListMedias.jsx
+++ front/src/pages/user/ListMedias.jsx
@@ -180,9 +180,9 @@ function ListMedias() {
                                   <p className="text-xs font-normal text-gray-500 dark:text-gray-400">
                                     {media.description
                                       ? media.description
-                                          .split(" ")
-                                          .splice(0, 5)
-                                          .join(" ") + "..."
+                                        .split(" ")
+                                        .splice(0, 5)
+                                        .join(" ") + "..."
                                       : "N/A"}
                                   </p>
                                 </div>
@@ -192,25 +192,22 @@ function ListMedias() {
 
                           <td className="px-12 py-4 text-sm font-medium text-slate-700 whitespace-nowrap">
                             <div
-                              className={`inline-flex items-center px-3 py-1 rounded-full gap-x-2 ${
-                                !media.is_deleted
+                              className={`inline-flex items-center px-3 py-1 rounded-full gap-x-2 ${!media.is_deleted
                                   ? "bg-emerald-600/60"
                                   : "bg-red-400/40"
-                              }`}
+                                }`}
                             >
                               <span
-                                className={`h-1.5 w-1.5 rounded-full ${
-                                  !media.is_deleted
+                                className={`h-1.5 w-1.5 rounded-full ${!media.is_deleted
                                     ? "bg-emerald-500"
                                     : "bg-red-500"
-                                }`}
+                                  }`}
                               ></span>
                               <h2
-                                className={`text-sm font-normal ${
-                                  !media.is_deleted
+                                className={`text-sm font-normal ${!media.is_deleted
                                     ? "text-emerald-500"
                                     : "text-red-500"
-                                }`}
+                                  }`}
                               >
                                 {!media.is_deleted
                                   ? "En ligne"
@@ -309,7 +306,7 @@ function ListMedias() {
         >
           <div
             onClick={(e) => e.stopPropagation()}
-            className="bg-gray-800 rounded p-8 w-4/12"
+            className="bg-gray-800 rounded p-8 xl:w-4/12"
           >
             <UploadMediaForm
               toggleModal={toggleModal}
@@ -327,7 +324,7 @@ function ListMedias() {
         >
           <div
             onClick={(e) => e.stopPropagation()}
-            className="bg-gray-800 rounded p-8 w-4/12"
+            className="bg-gray-800 rounded p-8 xl:w-4/12"
           >
             <DeleteMediaForm
               toggleModal={toggleModal}
@@ -346,7 +343,7 @@ function ListMedias() {
         >
           <div
             onClick={(e) => e.stopPropagation()}
-            className="bg-gray-800 rounded p-8 w-4/12"
+            className="bg-gray-800 rounded p-8 xl:w-4/12"
           >
             <UpdateMediaForm
               toggleModal={toggleModal}
